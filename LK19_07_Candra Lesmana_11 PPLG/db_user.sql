b05ab90c2f236907661b618627e97999db2e49d9
CandraLesmana <101246504+CandraLesmana@users.noreply.github.com>
2022-11-27 18:11:34 +0700

    Add files via upload

---
+++ LK19_07_Candra Lesmana_11 PPLG/db_user.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Nov 2022 pada 12.09
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_pendaftaran`
+--
+
+CREATE TABLE `tbl_pendaftaran` (
+  `id` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_pendaftaran`
+--
+
+INSERT INTO `tbl_pendaftaran` (`id`, `email`, `username`, `password`) VALUES
+('', 'candralesmana123@gmail.com', 'Candra ', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_pendaftaran`
+--
+ALTER TABLE `tbl_pendaftaran`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
